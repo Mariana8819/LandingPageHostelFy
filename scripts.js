147f620a6b85b76b562897629716d85f1bfccddc
Mariana8819 <mivallejos8819@gmail.com>
2024-08-18 22:14:46 -0300

    primer landingpage de HostelFy

---
+++ scripts.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function() {
+    const menu = document.querySelector('.menu');
+    if (window.scrollY > 0) {
+        menu.classList.add('scrolled');
+    } else {
+        menu.classList.remove('scrolled');
+    }
+});
+
+
+//Inicia EmailJS
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_eljxbcq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
